a5e9ae376a36e7f3e50b1273431925203cb11f3d
zhangy10 <zac.lhjzyzzoo@gmail.com>
2015-08-13 14:42:36 +1000

    local commit

---
+++ gen/com/example/hi_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.hi_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/zzoo/MainActivity.java
@@ -0,0 +1,18 @@
+package zzoo;
+
+import com.example.hi_android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		View v = this.findViewById(R.layout.main);
+		
+		setContentView(R.layout.main);
+		
+	}
+}
